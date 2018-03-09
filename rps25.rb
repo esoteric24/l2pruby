f9f61c1a18aa69605269bff9258de4bc5ca8f254
Student <student@Alexander-Witherspoons-MacBook-Air.local>
2018-03-09 15:22:17 -0500

    finished rps25

--- rps25.rb
+++ rps25.rb
@@ -26,17 +26,97 @@ a = 'gun'
 
 $aB = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y]
 
-#verbs
-#'attacks': a => y, a => x, a => p, n => g, n => e, s => r, s => q, s => n, s => g,
-# t => r, t => q, t => n
-#'creates': a => w, b => w, c => w, d => b, e => d, e => c, e => b, e => a, f => d,
-# f => w, g => d, p => j, p => i, q => p, r => q, r => p
-#'destroys': a => v, b => a, b => y, b => v, b => u, b => t, b => s, b => r, b => q,
-# b => p, b => o, c => b, c => a, c => y, c => v, c => u, d => a, d => v, d => u,
-# g => a, g => y, g => x, g => v, g => u, j => h, n => m, n => l, s => m, s => l,
-# t => m, u => m, w => v, w => u, w => m, w => l, y => m
-#'damages': a => u, m => l, v => u, v => p, v => m, v => l, x => v, x => u, x => m
+$vA = [' attacks ', ' creates ', ' destroys ', ' damages ', ' kills ',
+  ' covers ', 'stops', ' breaks ', ' hurts ', ' ignores ', ' uses ']
 
+$gunVerbs = {n => $vA[4], o => $vA[4], p => $vA[0], q => $vA[4], r => $vA[4],
+  s => $vA[4], t => $vA[4], u => $vA[3], v => $vA[2], w => $vA[1], x => $vA[0],
+  y => $vA[0]}
+  $dynaVerbs = {o => $vA[2], p => $vA[2], q => $vA[2], r => $vA[2], s => $vA[2],
+  t => $vA[2], u => $vA[2], v => $vA[2], w => $vA[1], x => $vA[5], y => $vA[2],
+  a => $vA[2]}
+  $nukeVerbs = {p => $vA[4], q => $vA[4], r => $vA[4], s => $vA[4], t => $vA[4],
+  u => $vA[2], v => $vA[2], w => $vA[1], x => $vA[5], y => $vA[2], a => $vA[2],
+  b => $vA[2]}
+  $lightVerbs = {q => $vA[8], r => $vA[8], s => $vA[4], t => $vA[4], u => $vA[2],
+  v => $vA[2], w => $vA[1], x => $vA[5], y => $vA[7], a => $vA[2], b => $vA[1],
+  c => $vA[6]}
+  $devilVerbs = {r => $vA[4], s => $vA[4], t => $vA[4], u => $vA[9],
+  v => $vA[9], w => $vA[1], x => $vA[2], y => $vA[2], a => $vA[1], b => $vA[1],
+  c => $vA[1], d => $vA[1]}
+  $dragonVerbs = {s => $vA[4], t => $vA[4], u => $vA[2], v => $vA[2],
+  w => $vA[1], x => $vA[5], y => $vA[7], a => $vA[2], b => $vA[9], c => $vA[9],
+  d => $vA[1], e => $vA[4]}
+  $alienVerbs = {t => $vA[4], u => $vA[2], v => $vA[2], w => $vA[6],
+  x => $vA[2], y => $vA[2], a => $vA[2], b => $vA[6], c => $vA[6], d => $vA[1],
+  e => $vA[9], f => $vA[4]}
+  $waterVerbs = {u => $vA[7], v => $vA[7], w => $vA[6], x => $vA[9],
+  y => $vA[7], a => $vA[7], b => $vA[6], c => $vA[6], d => $vA[9], e => $vA[8],
+  f => $vA[4], g => $vA[4]}
+  $bowlVerbs = {v => $vA[5], w => $vA[6], x => $vA[9], y => $vA[5], a => $vA[7],
+  b => $vA[6], c => $vA[6], d => $vA[9], e => $vA[8], f => $vA[4], g => $vA[4],
+  h => $vA[5]}
+  $airVerbs = {w => $vA[6], x => $vA[6], y => $vA[7], a => $vA[7], b => $vA[6],
+  c => $vA[6], d => $vA[1], e => $vA[4], f => $vA[4], g => $vA[4], h => $vA[2],
+  i => $vA[7]}
+  $moonVerbs = {x => $vA[5], y => $vA[2], a => $vA[9], b => $vA[6], c => $vA[9],
+  d => $vA[9], e => $vA[8], f => $vA[5], g => $vA[6], h => $vA[2], i => $vA[7],
+  j => $vA[9]}
+  $paperVerbs = {y => $vA[5], a => $vA[6], b => $vA[6], c => $vA[6],
+  d => $vA[6], e => $vA[7], f => $vA[9], g => $vA[9], h => $vA[5], i => $vA[5],
+  j => $vA[10], k => $vA[5]}
+  $spongeVerbs = {a => $vA[7], b => $vA[6], c => $vA[7], d => $vA[9],
+  e => $vA[8], f => $vA[9], g => $vA[8], h => $vA[10], i => $vA[10],
+  j => $vA[10], k => $vA[5], l => $vA[3]}
+  $wolfVerbs = {b => $vA[10], c => $vA[10], d => $vA[9], e => $vA[0],
+  f => $vA[0], g => $vA[0], h => $vA[10], i => $vA[10], j => $vA[10],
+  k => $vA[10], l => $vA[2], m => $vA[2]}
+  $roachVerbs = {c => $vA[9], d => $vA[9], e => $vA[9], f => $vA[9],
+  g => $vA[9], h => $vA[10], i => $vA[10], j => $vA[10], k => $vA[9],
+  l => $vA[2], m => $vA[2], n => $vA[9]}
+  $treeVerbs = {d => $vA[6], e => $vA[6], f => $vA[5], g => $vA[6],
+  h => $vA[10], i => $vA[1], j => $vA[1], k => $vA[5], l => $vA[1],
+  m => $vA[10], n => $vA[5], o => $vA[5]}
+  $childVerbs = {e => $vA[9], f => $vA[10], g => $vA[9], h => $vA[10],
+  i => $vA[10], j => $vA[10], k => $vA[10], l => $vA[10], m => $vA[10],
+  n => $vA[10], o => $vA[4], p => $vA[1]}
+  $adultVerbs = {f => $vA[4], g => $vA[9], h => $vA[10], i => $vA[10],
+  j => $vA[10], k => $vA[10], l => $vA[10], m => $vA[10], n => $vA[10],
+  o => $vA[4], p => $vA[1], q => $vA[1]}
+  $monkeyVerbs = {g => $vA[0], h => $vA[10], i => $vA[7], j => $vA[10],
+  k => $vA[9], l => $vA[2], m => $vA[2], n => $vA[0], o => $vA[4], p => $vA[10],
+  q => $vA[0], r => $vA[0]}
+  $snakeVerbs = {h => $vA[10], i => $vA[10], j => $vA[10], k => $vA[9],
+  l => $vA[0], m => $vA[2], n => $vA[0], o => $vA[4], p => $vA[10], q => $vA[0],
+  r => $vA[0], s => $vA[4]}
+  $axeVerbs = {i => $vA[7], j => $vA[9], k => $vA[9], l => $vA[2], m => $vA[2],
+  n => $vA[4], o => $vA[4], p => $vA[4], q => $vA[8], r => $vA[8], s => $vA[4],
+  t => $vA[4]}
+  $scisVerbs = {j => $vA[9], k => $vA[9], l => $vA[3], m => $vA[3], n => $vA[8],
+  o => $vA[4], p => $vA[3], q => $vA[8], r => $vA[8], s => $vA[4], t => $vA[4],
+  u => $vA[3]}
+  $fireVerbs = {k => $vA[5], l => $vA[2], m => $vA[2], n => $vA[8], o => $vA[4],
+  p => $vA[4], q => $vA[8], r => $vA[8], s => $vA[8], t => $vA[8], u => $vA[2],
+  v => $vA[2]}
+  $sunVerbs = {l => $vA[9], m => $vA[3], n => $vA[8], o => $vA[8], p => $vA[2],
+  q => $vA[8], r => $vA[8], s => $vA[8], t => $vA[8], u => $vA[3], v => $vA[3],
+  w => $vA[2]}
+  $rockVerbs = {m => $vA[2], n => $vA[8], o => $vA[4], p => $vA[6], q => $vA[8],
+  r => $vA[8], s => $vA[8], t => $vA[4], u => $vA[7], v => $vA[7], w => $vA[6],
+  x => $vA[5]}
+
+$vH = {a => $gunVerbs, b => $dynaVerbs, c => $nukeVerbs, d => $lightVerbs,
+  e => $devilVerbs, f => $dragonVerbs, g => $alienVerbs, h => $waterVerbs,
+  i => $bowlVerbs, j => $airVerbs, k => $moonVerbs, l => $paperVerbs,
+  m => $spongeVerbs, n => $wolfVerbs, o => $roachVerbs, p => $treeVerbs,
+  q => $childVerbs, r => $adultVerbs, s => $monkeyVerbs, t => $snakeVerbs,
+  u => $axeVerbs, v => $scisVerbs, w => $fireVerbs, x => $sunVerbs,
+  y => $rockVerbs}
+
+def findVerb (winner, loser)
+  $verbString = $vH[winner][loser]
+  puts winner.capitalize + $verbString + loser + '.'
+end
 
 def falsify range
   for $slotNum in range do
@@ -108,6 +188,11 @@ def determine tie
 end
 
 puts ''
+puts 'Welcome to RPS 25, where you play a version of Rock-Paper-Scissors with'
+puts 'twenty-five possible gestures instead of three. Input one of the following'
+puts 'gestures to see if you can defeat your opponent:'
+puts $aB.to_s.delete('[').delete(']').delete('"')
+puts ''
 puts 'Your gesture:'
 userGest = gets.chomp.downcase
 puts ''
@@ -123,6 +208,7 @@ else
   puts 'Your opponent\'s gesture:'
   puts comGest
   puts ''
+  userGest = $aB[$userSlot]
   determine $userSlot
 end
 
@@ -130,8 +216,10 @@ result = $aB[comSlot]
 
 if $checkResult == 'valid'
   if result == 'true'
+    findVerb userGest, comGest
     puts 'You won!'
   elsif result == 'false'
+    findVerb comGest, userGest
     puts 'You lost...'
   elsif result == 'tie'
     puts 'You tied.'
